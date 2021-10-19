1feb196095b55c645959dd712a02dd60f3eae0ed
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-10-19 16:05:21 -0600

    nav text correct

--- Composody/app/src/main/java/com/example/composody/ui/about/AboutFragment.kt
+++ Composody/app/src/main/java/com/example/composody/ui/about/AboutFragment.kt
@@ -1,4 +1,4 @@
-package com.example.composody.ui.slideshow
+package com.example.composody.ui.about
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -8,13 +8,12 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.composody.R
-import com.example.composody.databinding.FragmentSlideshowBinding
+import com.example.composody.databinding.FragmentAboutBinding
 
-class SlideshowFragment : Fragment() {
+class AboutFragment : Fragment() {
 
-    private lateinit var slideshowViewModel: SlideshowViewModel
-    private var _binding: FragmentSlideshowBinding? = null
+    private lateinit var aboutViewModel: AboutViewModel
+    private var _binding: FragmentAboutBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,14 +24,14 @@ class SlideshowFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        slideshowViewModel =
-            ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        aboutViewModel =
+            ViewModelProvider(this).get(AboutViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentAboutBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
+        val textView: TextView = binding.textAbout
+        aboutViewModel.text.observe(viewLifecycleOwner, Observer {
             textView.text = it
         })
         return root
--- Composody/app/src/main/java/com/example/composody/ui/about/AboutViewModel.kt
+++ Composody/app/src/main/java/com/example/composody/ui/about/AboutViewModel.kt
@@ -1,13 +1,13 @@
-package com.example.composody.ui.slideshow
+package com.example.composody.ui.about
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class AboutViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is slideshow Fragment"
+        value = "This is About App Fragment"
     }
     val text: LiveData<String> = _text
 }

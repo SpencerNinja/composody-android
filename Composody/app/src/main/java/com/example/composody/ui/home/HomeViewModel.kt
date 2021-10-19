76bc7f454b6cec77a2670f48f083d5a4d16c76dd
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-10-19 16:23:53 -0600

    added background image to activity_main

--- Composody/app/src/main/java/com/example/composody/ui/home/HomeViewModel.kt
+++ Composody/app/src/main/java/com/example/composody/ui/home/HomeViewModel.kt
@@ -1,8 +1,16 @@
 package com.example.composody.ui.home
 
+import android.os.CountDownTimer
+import android.util.Log
+import android.view.View
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.composody.Note
+import com.example.composody.R
+import com.example.composody.Scale
+import com.karlotoy.perfectune.instance.PerfectTune
+import kotlin.random.Random
 
 class HomeViewModel : ViewModel() {
 
@@ -10,4 +18,5 @@ class HomeViewModel : ViewModel() {
         value = "This is home Fragment"
     }
     val text: LiveData<String> = _text
+
 }

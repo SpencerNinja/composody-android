6c45cb3c50d89cbfcbefc3371e18ee1450cad81d
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-10-19 15:43:34 -0600

    added notes and scales

---
+++ Composody/app/src/main/java/com/example/composody/Note.kt
@@ -0,0 +1,31 @@
+package com.example.composody
+
+import com.karlotoy.perfectune.instance.PerfectTune
+import kotlin.random.Random
+
+class Note() {
+    var frequency: Double
+    var toneObject: PerfectTune
+    var duration: Int
+
+    init {
+        frequency = 0.0
+        toneObject = PerfectTune()
+        duration = 1000
+    }
+
+    fun assignDuration() {
+        var randomDuration = Random.nextInt(500, 2000)
+        duration = randomDuration
+    }
+
+    fun assignFrequency() {
+        toneObject.tuneFreq = frequency
+    }
+
+    fun playFreq() {
+        toneObject.tuneAmplitude = 10000
+        toneObject.playTune()
+    }
+
+}

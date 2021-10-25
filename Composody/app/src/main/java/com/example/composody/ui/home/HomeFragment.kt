2551a8108dce823b13b69bc696747f34ddb86ddd
SpencerNinja <62817123+SpencerNinja@users.noreply.github.com>
2021-10-25 16:50:50 -0600

    note picker fixed

--- Composody/app/src/main/java/com/example/composody/ui/home/HomeFragment.kt
+++ Composody/app/src/main/java/com/example/composody/ui/home/HomeFragment.kt
@@ -1,24 +1,14 @@
 package com.example.composody.ui.home
 
 import android.os.Bundle
-import android.os.CountDownTimer
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.widget.NumberPicker
-import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
-import com.example.composody.Note
 import com.example.composody.R
-import com.example.composody.Scale
 import com.example.composody.databinding.FragmentHomeBinding
-import com.google.android.material.snackbar.Snackbar
-import com.karlotoy.perfectune.instance.PerfectTune
-import kotlin.random.Random
 
 class HomeFragment : Fragment() {
 
@@ -34,28 +24,39 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-//        // NumberPicker
-//        var data = listOf("2", "3", "4", "5", "6", "7", "8", "9", "10")
-//        var picker = binding.numberPicker
-//        picker.minValue = data[0].toInt()
-//        picker.maxValue = data.size
-//        picker.displayedValues = data.toTypedArray()
-
         homeViewModel =
             ViewModelProvider(this).get(HomeViewModel::class.java)
 
+        // Inflate view
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
+        // OnClickListener for "Generate" melody button
         root.findViewById<Button>(R.id.button_generate_melody).setOnClickListener {
             // Create the melody
             homeViewModel.createMelody()
         }
 
+        // Notes (How many notes?) NumberPicker
+        var noteCount = listOf("3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13")
+        var notePicker = binding.numberPicker
+        notePicker.minValue = noteCount[0].toInt()
+        notePicker.maxValue = noteCount.size
+        notePicker.displayedValues = noteCount.toTypedArray()
+
+        // Scale (What scale to pull notes from?) NumberPicker
+        var data2 = listOf("3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13")
+        var picker2 = binding.numberPicker
+        picker2.minValue = data2[0].toInt()
+        picker2.maxValue = data2.size
+        picker2.displayedValues = data2.toTypedArray()
+
+        // Display text depending on current page
 //        val textView: TextView = binding.textHome
 //        homeViewModel.text.observe(viewLifecycleOwner, Observer {
 //            textView.text = it
 //        })
+
         return root
     }
 
